7db8f09f4acee020b6bccd80ad7275f024dd49a7
Dong Yoo Kang(Xellos00) <xellos.kang@gmail.com>
2021-06-21 23:09:44 +0900

    Update grafana - if there's no instance_id that can not be specified - unavailable resource

--- src/spaceone/monitoring/manager/event_manager.py
+++ src/spaceone/monitoring/manager/event_manager.py
@@ -6,7 +6,8 @@ from spaceone.monitoring.model.event_response_model import EventModel
 
 _LOGGER = logging.getLogger(__name__)
 _INTERVAL_IN_SECONDS = 600
-_EXCEPTION_TO_PASS = ["Test notification", "null", "Null"]
+_EXCEPTION_TO_PASS = ["Test notification"]
+
 
 class EventManager(BaseManager):
     def __init__(self, *args, **kwargs):
@@ -14,28 +15,28 @@ class EventManager(BaseManager):
 
     def parse(self, options, raw_data):
 
-        # check test notification for skip
-        #"ruleName": "Test notification", "title": "[Alerting] Test notification", "ruleUrl": "https://grafana.stargate.cloudeco.io/"
-
         default_parsed_data = []
         eval_match_values = raw_data.get('evalMatches', [])
         occurred_at = datetime.now()
+        event_key = self._get_event_key(raw_data, occurred_at)
+        event_type = self._get_event_type(raw_data)
+        severity = self._get_severity(raw_data)
+        description = raw_data.get('message', '')
+        title = raw_data.get('title', '')
+        rule_name = raw_data.get('ruleName', '')
+        # Check Eval Matched Value is Empty
 
-        for eval_match_value in eval_match_values:
-            target_types, title, instance_id = self._get_alarm_title_type_instance_id(raw_data, eval_match_value)
-
-            event_key = self._get_event_key(raw_data,  occurred_at, instance_id)
-            event_resource = self._get_resource_for_event(eval_match_value, {}, target_types, instance_id, raw_data)
+        if len(eval_match_values) == 0:
 
             event_vo = {
                 'event_key': event_key,
-                'event_type': self._get_event_type(raw_data),
-                'severity': self._get_severity(raw_data),
-                'resource': event_resource,
-                'description': raw_data.get('message', ''),
+                'event_type': event_type,
+                'severity': severity,
                 'title': title,
+                'rule': rule_name,
+                'resource': {},
+                'description': description,
                 'occurred_at': occurred_at,
-                'rule': self._get_rule_for_event(raw_data),
                 'additional_info': self._get_additional_info(raw_data)
             }
 
@@ -45,26 +46,47 @@ class EventManager(BaseManager):
             event_result_model_primitive = event_result_model.to_native()
             default_parsed_data.append(event_result_model_primitive)
 
+        else:
+
+            for eval_match_value in eval_match_values:
+
+                event_resource = self._get_resource_for_event(eval_match_value, raw_data)
+
+                event_vo = {
+                    'event_key': event_key,
+                    'event_type': self._get_event_type(raw_data),
+                    'severity': self._get_severity(raw_data),
+                    'title': title,
+                    'rule': rule_name,
+                    'description': description,
+                    'resource': event_resource,
+                    'occurred_at': occurred_at,
+                    'additional_info': self._get_additional_info(raw_data)
+                }
+
+                _LOGGER.debug(f'[EventManager] parse Event : {event_vo}')
+                event_result_model = EventModel(event_vo, strict=False)
+                event_result_model.validate()
+                event_result_model_primitive = event_result_model.to_native()
+                default_parsed_data.append(event_result_model_primitive)
+
         return default_parsed_data
 
     @staticmethod
-    def _get_rule_for_event(raw_data):
-        return raw_data.get('ruleName')
-
-    @staticmethod
-    def _get_event_key(raw_data, occurred_at, instance_id):
+    def _get_event_key(raw_data, occurred_at):
         dashboard_id = raw_data.get('dashboardId')
         panel_id = raw_data.get('panelId')
         rule_id = raw_data.get('ruleId')
+        org_id = raw_data.get('orgId')
         indexed_unique_key = None
 
         if isinstance(occurred_at, datetime):
             occurred_at_timestamp = str(occurred_at.timestamp())
             _occurred_at_timestamp = occurred_at_timestamp[:occurred_at_timestamp.find('.')]
             str_to_int = float(_occurred_at_timestamp) if len(_occurred_at_timestamp) > 10 else int(_occurred_at_timestamp)
-            indexed_unique_key = int(str_to_int) // _INTERVAL_IN_SECONDS * 100
+            indexed_unique_key = int(str_to_int) // _INTERVAL_IN_SECONDS
 
-        raw_event_key = f'{dashboard_id}:{panel_id}:{rule_id}:{instance_id}:{indexed_unique_key}'
+        raw_event_key = f'{dashboard_id}:{panel_id}:{rule_id}:{org_id}:{indexed_unique_key}'
         hash_object = hashlib.md5(raw_event_key.encode())
         md5_hash = hash_object.hexdigest()
 
@@ -72,7 +94,14 @@ class EventManager(BaseManager):
 
     @staticmethod
     def _get_event_type(raw_data):
-        event_state = raw_data.get('state', 'no_data')
+        """
+        alerting: ALERT
+        no_data: ALERT
+        pending: ALERT
+        ok: RECOVERY
+        paused: ALERT
+        """
+        event_state = raw_data.get('state')
         return 'RECOVERY' if event_state == 'ok' else 'ALERT'
 
     @staticmethod
@@ -81,49 +110,48 @@ class EventManager(BaseManager):
         default_severity_flag = 'NOT_AVAILABLE'
         if event_state == 'ok':
             default_severity_flag = 'INFO'
-        elif event_state == 'alerting':
+        elif event_state in ['alerting', 'pending', 'paused']:
             default_severity_flag = 'ERROR'
 
         return default_severity_flag
 
     @staticmethod
     def _get_additional_info(raw_data):
-        tags = {}
 
-        if 'imageUrl' in raw_data:
-            tags.update({'image_url': raw_data.get('imageUrl')})
+        additional_info = {}
+        if 'dashboardId' in raw_data:
+            additional_info.update({'dashboard_id': str(raw_data.get('dashboardId', ''))})
+
+        if 'panelId' in raw_data:
+            additional_info.update({'panel_id': str(raw_data.get('panelId'))})
 
         if 'orgId' in raw_data:
-            tags.update({'org_id': str(raw_data.get('orgId', ''))})
+            additional_info.update({'org_id': str(raw_data.get('orgId', ''))})
+
+        if 'imageUrl' in raw_data:
+            additional_info.update({'image_url': raw_data.get('imageUrl')})
 
         if 'ruleUrl' in raw_data:
-            tags.update({'rule_url': raw_data.get('ruleUrl')})
+            additional_info.update({'rule_url': raw_data.get('ruleUrl')})
 
-        if 'dashboardId' in raw_data:
-            tags.update({'dashboard_id': str(raw_data.get('dashboardId', ''))})
-
-        return tags
+        return additional_info
 
     @staticmethod
-    def _get_resource_for_event(eval_value, event_resource, resource_type, instance_id, raw_data):
-        tags = eval_value.get('tags', {})
+    def _get_resource_for_event(eval_value, raw_data):
+        event_resource = {}
+        tags = eval_value.get('tags')
         if tags is None:
-            dashboard_id = str(raw_data.get('dashboardId', ''))
-            panel_id = str(raw_data.get('panelId', ''))
-            org_id = str(raw_data.get('orgId', ''))
-            _instance_id = f'{dashboard_id}{panel_id}{org_id}'
-            if instance_id == _instance_id:
-                event_resource.update({
-                    'resource_id': f'[{resource_type}], Dashboard ID: {dashboard_id}, Panel ID: {panel_id}',
-                    'resource_type': resource_type,
-                })
+            event_resource.update({'name': 'unavailable resource'})
         else:
+            resource_types = [tag for tag in tags]
             for idx, tag in enumerate(tags):
                 if idx == 0:
-                    event_id = tags.get(tag)
+                    resource_id = tags.get(tag)
+                    resource_type = tag
                     event_resource.update({
-                        'resource_type': resource_type,
-                        'resource_id': event_id
+                        'name': f'[{resource_type}]:{resource_id}',
+                        'resource_type': '&'.join(resource_types),
+                        'resource_id': resource_id
                     })
                     break
 
@@ -136,35 +164,3 @@ class EventManager(BaseManager):
         if rule_name in _EXCEPTION_TO_PASS:
             notification_state_to_process = True
         return notification_state_to_process
-
-    @staticmethod
-    def _is_invalid_to_proceed(eval_value):
-        tags = eval_value.get('tags')
-        is_invalid = True
-
-        if tags is None or tags in _EXCEPTION_TO_PASS:
-            pass
-        else:
-            is_invalid = False
-
-        return is_invalid
-
-    @staticmethod
-    def _get_alarm_title_type_instance_id(raw_data, eval_value):
-        tags = eval_value.get('tags', {})
-        title = raw_data.get('title', '')
-        target_types = []
-        instance_id = None
-        if tags is None:
-            dashboard_id = str(raw_data.get('dashboardId', ''))
-            panel_id = str(raw_data.get('panelId', ''))
-            org_id = str(raw_data.get('orgId', ''))
-            instance_id = f'{dashboard_id}{panel_id}{org_id}'
-            return 'UNKNOWN', title, instance_id
-        else:
-            for idx, tag in enumerate(tags):
-                if idx == 0:
-                    instance_id = tags.get(tag)
-                target_types.append(tag)
-
-            return '' if len(target_types) == 0 else '&'.join(target_types), title, instance_id
--- test/api/test_event_api.py
+++ test/api/test_event_api.py
@@ -9,17 +9,63 @@ from pprint import pprint
 _LOGGER = logging.getLogger(__name__)
 TEST_JSON = os.environ.get('test_json', None)
 
-def _get_json():
-    with open(TEST_JSON) as json_file:
-        json_data = json.load(json_file)
-        return json_data
 
 class TestEvent(TestCase):
 
     def test_parse(self):
-        params = _get_json()
-        """
-        {
+        params1 = {
+            "options": {
+
+            },
+            "data": {
+                "tags": {
+
+                },
+                "evalMatches": [
+
+                ],
+                "ruleUrl": "https://grafana.stargate.cloudeco.io/d/uZaspace/spaceone-dev-cluster-alerts-dashboard?tab=alert&viewPanel=58&orgId=1",
+                "ruleId": 57.0,
+                "ruleName": "Not Running Pods 0:OK alert",
+                "message": "[cloudone-dev-v1-eks-cluster] Not Running Pods 0 is OK\n\nFailure level : WorkerNode\nPanel : Not Running Pods 0:OK\nDataSource : Prometheus\nResource : pod\nThreshold : not running pod count > 0 , every 5m , for 5m",
+                "imageUrl": "https://grafana.stargate.cloudeco.io/public/img/attachments/GjadpyvgFPVwSGtrfeww.png",
+                "dashboardId": 10.0,
+                "state": "ok",
+                "orgId": 1.0,
+                "panelId": 58.0,
+                "title": "[OK] Not Running Pods 0:OK alert"
+            }
+        }
+        params2 = {
+            "options": {
+
+            },
+            "data": {
+                "evalMatches": [
+                    {
+                        "tags": {
+                            "pod": "plugin-grafana-monitoring-webhook-rajbcnsjbhjszvfv-8b84876q9wq9"
+                        },
+                        "metric": "plugin-grafana-monitoring-webhook-rajbcnsjbhjszvfv-8b84876q9wq9",
+                        "value": 0.15384615384615385
+                    }
+                ],
+                "imageUrl": "https://grafana.stargate.cloudeco.io/public/img/attachments/yo4maLzA629Oh24c1g1g.png",
+                "ruleId": 57.0,
+                "title": "[Alerting] Not Running Pods 0:OK alert",
+                "panelId": 58.0,
+                "state": "alerting",
+                "orgId": 1.0,
+                "ruleName": "Not Running Pods 0:OK alert",
+                "ruleUrl": "https://grafana.stargate.cloudeco.io/d/uZaspace/spaceone-dev-cluster-alerts-dashboard?tab=alert&viewPanel=58&orgId=1",
+                "dashboardId": 10.0,
+                "message": "[cloudone-dev-v1-eks-cluster] Not Running Pods 0 is OK\n\nFailure level : WorkerNode\nPanel : Not Running Pods 0:OK\nDataSource : Prometheus\nResource : pod\nThreshold : not running pod count > 0 , every 5m , for 5m",
+                "tags": {
+
+                }
+            }
+        }
+        params3 = {
             "options": {
 
             },
@@ -34,19 +80,24 @@ class TestEvent(TestCase):
                     {
                         "value": 0.48198821648077433,
                         "metric": "{}",
-                        "tags": null
+                        "tags": None
                     }
                 ],
                 "title": "[Alerting] API Server Request Latency TEMP",
                 "tags": {
+
                 },
                 "panelId": 102.0,
                 "state": "alerting",
                 "ruleUrl": "https://grafana.stargate.cloudeco.io/d/uZaspace/spaceone-dev-cluster-alerts-dashboard?tab=alert&viewPanel=102&orgId=1"
             }
         }
-        """
-        parsed_data = self.monitoring.Event.parse({'options': params.get('options'), 'data': params.get('data')})
+
+        parsed_data = self.monitoring.Event.parse({'options': {}, 'data': params1.get('data')})
+        print_json(parsed_data)
+        parsed_data = self.monitoring.Event.parse({'options': {}, 'data': params2.get('data')})
+        print_json(parsed_data)
+        parsed_data = self.monitoring.Event.parse({'options': {}, 'data': params3.get('data')})
         print_json(parsed_data)
 
 
