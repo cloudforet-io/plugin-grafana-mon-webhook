f4e1b938e34dd14d599f2bbc1d08ab0d37c56556
jean1042 <jean1042@naver.com>
2021-10-01 16:56:06 +0900

    refactor: add image_url to eventInfo model

--- src/spaceone/monitoring/info/event_info.py
+++ src/spaceone/monitoring/info/event_info.py
@@ -6,20 +6,21 @@ from spaceone.core.pygrpc.message_type import *
 __all__ = ['EventInfo', 'EventsInfo']
 
 
-def EventInfo(event_Info_data: EventModel):
+def EventInfo(event_data: EventModel):
     info = {
-        'event_key': event_Info_data['event_key'],
-        'event_type': event_Info_data['event_type'],
-        'description': event_Info_data.get('description'),
-        'title': event_Info_data['title'],
-        'severity': event_Info_data['severity'],
-        'resource': change_struct_type(event_Info_data['resource']),
-        'rule': event_Info_data.get('rule'),
-        'occurred_at': utils.datetime_to_iso8601(event_Info_data.get('occurred_at')),
-        'additional_info': change_struct_type(event_Info_data.get('additional_info'))
+        'event_key': event_data['event_key'],
+        'event_type': event_data['event_type'],
+        'description': event_data.get('description'),
+        'title': event_data['title'],
+        'image_url': event_data.get('image_url'),
+        'severity': event_data['severity'],
+        'resource': change_struct_type(event_data['resource']),
+        'rule': event_data.get('rule'),
+        'occurred_at': utils.datetime_to_iso8601(event_data.get('occurred_at')),
+        'additional_info': change_struct_type(event_data.get('additional_info'))
     }
     return event_pb2.EventInfo(**info)
 
 
-def EventsInfo(event_Info_datas, **kwargs):
-    return event_pb2.EventsInfo(results=list(map(functools.partial(EventInfo, **kwargs), event_Info_datas)))
+def EventsInfo(event_datas, **kwargs):
+    return event_pb2.EventsInfo(results=list(map(functools.partial(EventInfo, **kwargs), event_datas)))
